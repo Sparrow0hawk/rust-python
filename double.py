c7addb6441e93a168848727693e77fb90f47da21
sparrow0hawk <alexjcoleman@gmail.com>
2021-06-06 21:14:32 +0100

    adding a numpy implementation and badly configured numba

--- double.py
+++ double.py
@@ -1,4 +1,6 @@
-from numba import jit
+from numba import jit, types
+import numba as nb
+import numpy as np
 
 def count_doubles(val):
 
@@ -8,11 +10,28 @@ def count_doubles(val):
             total += 1
     return total
 
-@jit(nopython=True)
-def count_doubles_jit(val):
+def count_doubles_numpy(val):
 
-    total = 0 
-    for c1, c2 in zip(val, val[1:]):
-        if c1 == c2:
-            total += 1
-    return total
+    byte_array = np.frombuffer(val.encode(), np.byte)
+    return np.sum(byte_array[:-1] == byte_array[1:])
+
+@jit(nopython = True)
+def count_doubles_numpy_jit(val):
+
+    val_array = []
+
+    for idx in range(len(val)):
+        val_array.append(ord(val[idx]))
+
+    byte_array = np.array(val_array, dtype=np.int8)
+    return np.sum(byte_array[:-1] == byte_array[1:])
+
+def count_doubles_numpy_loop(val):
+
+    val_array = []
+
+    for idx in range(len(val)):
+        val_array.append(ord(val[idx]))
+
+    byte_array = np.array(val_array, dtype=np.int8)
+    return np.sum(byte_array[:-1] == byte_array[1:])
--- test-double.py
+++ test-double.py
@@ -9,12 +9,17 @@ val = ''.join(random.choice(string.ascii_letters) for i in range(1000000))
 def test_double_python(benchmark):
     benchmark(double.count_doubles, val)
 
-def test_double_jit(benchmark):
-    benchmark(double.count_doubles_jit, val)
-
-
 def test_double_rust(benchmark):
     benchmark(myrustlib.count_doubles, val)
 
 def test_cdouble(benchmark):
     benchmark(cdouble.count_doubles, val)
+
+def test_double_np(benchmark):
+    benchmark(double.count_doubles_numpy, val)
+
+def test_double_np_jit(benchmark):
+    benchmark(double.count_doubles_numpy_jit, val)
+
+def test_count_doubles_numpy_loop(benchmark):
+    benchmark(double.count_doubles_numpy_loop, val)
