995f75c29316c4d210b55cb4ed8b839ac42181cb
sparrow0hawk <alexjcoleman@gmail.com>
2021-06-06 18:44:44 +0100

    added numba version and updated test file

--- double.py
+++ double.py
@@ -1,5 +1,15 @@
+from numba import jit
 
 def count_doubles(val):
+
+    total = 0 
+    for c1, c2 in zip(val, val[1:]):
+        if c1 == c2:
+            total += 1
+    return total
+
+@jit(nopython=True)
+def count_doubles_jit(val):
 
     total = 0 
     for c1, c2 in zip(val, val[1:]):
--- test-double.py
+++ test-double.py
@@ -2,11 +2,19 @@ import double
 import random
 import string
 import myrustlib 
+import cdouble
 
 val = ''.join(random.choice(string.ascii_letters) for i in range(1000000))
 
 def test_double_python(benchmark):
     benchmark(double.count_doubles, val)
 
+def test_double_jit(benchmark):
+    benchmark(double.count_doubles_jit, val)
+
+
 def test_double_rust(benchmark):
     benchmark(myrustlib.count_doubles, val)
+
+def test_cdouble(benchmark):
+    benchmark(cdouble.count_doubles, val)
